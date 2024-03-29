70da7d11f38ebb8dd7c4c1808d4df037c766ac3c
Preethi-Evelyn-Sadanandan <preethievelynsadanandan@gmail.com>
2024-03-29 20:59:56 +0530

    Simplified location_dim since PK column removed

--- models/analytics/location_dim.sql
+++ models/analytics/location_dim.sql
@@ -1,33 +1,16 @@
 {{ 
     config(
-        materialized='incremental',
-        incremental_strategy='delete+insert',
-        unique_key='location_sk',
+        materialized='table'
     ) 
 }}
 
 
 SELECT
-    CASE 
-        WHEN sub.op_type = 'C' THEN sub.location_id 
-        ELSE tgt.location_sk
-    END AS location_sk
-    , CASE 
-        WHEN sub.op_type IN ('C', 'U') THEN sub.district_name
-        ELSE tgt.district
-    END AS district
-    , CASE 
-        WHEN sub.op_type IN ('C', 'U') THEN sub.block_name 
-        ELSE tgt.block 
-    END AS block
-    , CASE 
-        WHEN sub.op_type IN ('C', 'U') THEN sub.gp_name
-        ELSE tgt.gram_panchayat
-    END AS gram_panchayat
-    , CASE 
-        WHEN sub.op_type IN ('C', 'U') THEN sub.ward_name 
-        ELSE tgt.ward
-    END AS ward
+    sub.location_id AS location_sk
+    , sub.district_name AS district
+    , sub.block_name AS block
+    , sub.gp_name AS gram_panchayat
+    , sub.ward_name AS ward
     , CASE 
         WHEN sub.op_type='C' THEN CURRENT_TIMESTAMP
         ELSE tgt.create_db_timestamp
@@ -50,8 +33,3 @@ SELECT
 FROM
     {{ ref ('location_cdc') }} AS sub
 LEFT JOIN {{ source ('analytics', 'location_dim') }} AS tgt ON sub.location_id = tgt.location_sk
-
-{% if is_incremental() %}
-    WHERE sub.src_last_modified_at >= (SELECT MAX(last_updated_timestamp) FROM {{ this }})
-{% endif %}
-
