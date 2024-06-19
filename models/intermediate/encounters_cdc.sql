1d65714d79432ef700b7b6c6174142df6ff465af
Abhishek-N <abhisheknshetty97@gmail.com>
2024-06-19 15:55:28 +0530

    add cdc models

--- models/intermediate/encounters_cdc.sql
+++ models/intermediate/encounters_cdc.sql
@@ -1,38 +1,54 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='id',
-        incremental_strategy='delete+insert',
-    )
-}}
+{{ config(
+    materialized='table'
+) }}
+
+WITH source AS (
+    SELECT
+        "ID" as id,
+        audit,
+        "Voided" as voided,
+        "Subject_ID" as subject_id,
+        "External_ID" as external_id,
+        "Subject_type" as subject_type,
+        observations,
+        "Encounter_type" as encounter_type,
+        "Cancel_location" as cancel_location,
+        to_timestamp("Cancel_date_time", 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as cancel_date_time,
+        to_timestamp(last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as last_modified_at,
+        "Encounter_location" as encounter_location,
+        to_timestamp("Max_scheduled_date", 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as max_scheduled_date,
+        "cancelObservations" as cancel_observations,
+        to_timestamp("Encounter_date_time", 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as encounter_date_time,
+        "Subject_external_ID" as subject_external_id,
+        to_timestamp("Earliest_scheduled_date", 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as earliest_scheduled_date,
+        _airbyte_raw_id,
+        _airbyte_extracted_at,
+        _airbyte_meta,
+        CASE 
+            WHEN to_timestamp(last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') = to_timestamp(json_extract_path_text(audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'C'
+            WHEN to_timestamp(last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') >= to_timestamp(json_extract_path_text(audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'U'
+            ELSE 'NA'
+        END AS op_type
+    FROM {{ source('source_arghyam_surveys', 'encounters') }}
+)
 
 SELECT
-    enc.id
-    , enc._airbyte_ab_id AS airbyte_raw_id 
-    , TO_TIMESTAMP(enc.max_scheduled_date, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') AS max_scheduled_date
-    , TO_TIMESTAMP(enc.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') AS last_modified_at
-    , enc.subject_external_id
-    , enc.audit
-    , TO_TIMESTAMP(enc.cancel_date_time, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') AS cancel_date_time
-    , enc.cancel_location
-    , TO_TIMESTAMP(enc.earliest_scheduled_date, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') AS earliest_scheduled_date
-    , TO_TIMESTAMP(enc.encounter_date_time, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') AS encounter_date_time
-    , enc.encounter_type
-    , enc.encounter_location
-    , enc.observations
-    , enc.external_id
-    , enc.voided
-    , enc.cancelobservations
-    , enc.subject_type
-    , enc.subject_id
-    , CASE 
-        WHEN TO_TIMESTAMP(enc.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') = TO_TIMESTAMP(json_extract_path_text(enc.audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'C'
-        WHEN TO_TIMESTAMP(enc.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') >= TO_TIMESTAMP(json_extract_path_text(enc.audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'U'
-        -- How should we handle cancellations?
-        ELSE 'NA'
-    END AS op_type
-FROM {{ source('silver', 'encounters_normalized') }} AS enc
-
-{% if is_incremental() %}
-  WHERE TO_TIMESTAMP(enc.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') >= (SELECT MAX(last_modified_at) FROM {{ this }})
-{% endif %}
+    id,
+    audit,
+    voided,
+    subject_id,
+    external_id,
+    subject_type,
+    observations,
+    encounter_type,
+    cancel_location,
+    cancel_date_time,
+    last_modified_at,
+    encounter_location,
+    max_scheduled_date,
+    cancel_observations,
+    encounter_date_time,
+    subject_external_id,
+    earliest_scheduled_date,
+    op_type
+FROM source
--- models/intermediate/subjects_cdc.sql
+++ models/intermediate/subjects_cdc.sql
@@ -1,32 +1,53 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='id',
-        incremental_strategy='delete+insert',
-    )
-}}
+{{ config(
+    materialized='table',
+) }}
+
+WITH source AS (
+    SELECT
+        "ID" as id,
+        audit,
+        "Groups" as groups,
+        "Voided" as voided,
+        location,
+        relatives,
+        catchments,
+        encounters,
+        enrolments,
+        "External_ID" as external_id,
+        "Subject_type" as subject_type,
+        observations,
+        to_timestamp(last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as last_modified_at,
+        to_timestamp("Registration_date", 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') as registration_date,
+        "Registration_location" as registration_location,
+        _airbyte_raw_id,
+        _airbyte_extracted_at,
+        _airbyte_meta,
+        CASE 
+            WHEN to_timestamp(last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') = to_timestamp(json_extract_path_text(audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'C'
+            WHEN to_timestamp(last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') >= to_timestamp(json_extract_path_text(audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'U'
+            ELSE 'NA'
+        END AS op_type
+    FROM {{ source('source_arghyam_surveys', 'subjects') }}
+)
 
 SELECT
-    sub.id 
-    , sub._airbyte_ab_id AS airbyte_raw_id 
-    , sub.catchments 
-    , sub.encounters 
-    , sub.voided 
-    , sub.registration_location 
-    , TO_TIMESTAMP(sub.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') AS last_modified_at
-    , sub.location 
-    , sub.audit 
-    , sub.observations 
-    , CAST(sub.registration_date AS DATE) AS registration_date
-    , CASE 
-        WHEN TO_TIMESTAMP(sub.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') = TO_TIMESTAMP(json_extract_path_text(sub.audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'C'
-        WHEN TO_TIMESTAMP(sub.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') >= TO_TIMESTAMP(json_extract_path_text(sub.audit::json, 'Created at'), 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') THEN 'U'
-        -- How are deletes handled?
-        ELSE 'NA'
-    END AS op_type
-FROM {{ source('silver', 'subjects_normalized') }} AS sub
--- LEFT JOIN {{ source('cdc_models', 'subjects_cdc') }} AS tgt ON tgt.airbyte_raw_id = sub._airbyte_ab_id
-
-{% if is_incremental() %}
-  WHERE TO_TIMESTAMP(sub.last_modified_at, 'YYYY-MM-DD"T"HH24:MI:SS.US"T"TZ') >= (SELECT MAX(last_modified_at) FROM {{ this }})
-{% endif %}
+    id,
+    audit,
+    groups,
+    voided,
+    location,
+    relatives,
+    catchments,
+    encounters,
+    enrolments,
+    external_id,
+    subject_type,
+    observations,
+    last_modified_at,
+    registration_date,
+    registration_location,
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
+    _airbyte_meta,
+    op_type
+FROM source
