808a25b40bb2790f14e098cebd2285df9e18d00c
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-08-16 13:34:31 +0530

    moved the columns of payments into paymentdetails, bills into billdetails, demands into demanddetails

--- models/intermediate/mgramseva/billdetails.sql
+++ models/intermediate/mgramseva/billdetails.sql
@@ -4,26 +4,40 @@
 
 WITH flattened AS (
   select 
-    jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'tenantId' as tenantid,
-    jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'demandId' as demandid,
-    jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'billId' as billid,
+    "data"->>'tenantId' as tenantid,
+    "data"->>'consumerCode' as consumercode,
+    "data"->>'id' as billid,
+    "data"->>'billNumber' as billnumber,
+    to_timestamp(
+      ROUND(
+        ("data"->>'billDate')::numeric * 0.001
+      )
+    ) AT TIME ZONE 'UTC' AS billdate,
+    ("data"->>'totalAmount')::numeric as totalbillamount,
+    "data"->>'mobileNumber' as mobilenumber,
     jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'id' as billdetailid,
+    jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'demandId' as demandid,
     jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'toPeriod' as toperiod,
     jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'fromPeriod' as fromperiod,
-    jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'amount' as amount
+    jsonb_array_elements(("data"->>'billDetails')::jsonb)->>'amount' as billamount
   from {{ source('source_mgramseva', 'bills') }}
 )
 
 select 
   tenantid, 
+  consumercode,
   demandid,
   billid, 
   billdetailid, 
+  billnumber,
+  billdate,
+  totalbillamount,
+  mobilenumber,
   to_timestamp(
 		ROUND(toperiod::numeric * 0.001)
-	) AT TIME ZONE 'UTC' AS toperiod,
+	) AT TIME ZONE 'UTC' AS billtoperiod,
 	to_timestamp(
 		ROUND(fromperiod::numeric * 0.001)
-	) AT TIME ZONE 'UTC' AS fromperiod, 
-  amount::numeric as amount
+	) AT TIME ZONE 'UTC' AS billfromperiod, 
+  billamount::numeric as billamount
 from flattened
--- models/intermediate/mgramseva/demanddetails.sql
+++ models/intermediate/mgramseva/demanddetails.sql
@@ -4,16 +4,27 @@
 
 WITH flattened AS (
   select 
-    jsonb_array_elements(("data"->>'demandDetails')::jsonb)->>'tenantId' as tenantid,
-    jsonb_array_elements(("data"->>'demandDetails')::jsonb)->>'demandId' as demandid,
+    "data"->>'id' as demandid,
+    "data"->>'status' as demandstatus,
+    "data"->>'tenantId' as tenantid,
+    "data"->>'consumerCode' as consumercode,
+    to_timestamp(
+      ROUND(
+        ("data"->'auditDetails'->>'createdTime')::numeric * 0.001
+      )
+    ) AT TIME ZONE 'UTC' AS demandcreatedtime,
+
     jsonb_array_elements(("data"->>'demandDetails')::jsonb)->>'id' as demanddetailid,
-    jsonb_array_elements(("data"->>'demandDetails')::jsonb)->>'taxAmount' as amount
+    jsonb_array_elements(("data"->>'demandDetails')::jsonb)->>'taxAmount' as demandamount
   from {{ source('source_mgramseva', 'demands') }}
 )
 
 SELECT 
   tenantid, 
   demandid,
+  demandstatus,
+  consumercode,
+  demandcreatedtime,
   demanddetailid, 
-  amount::numeric as amount
+  demandamount::numeric as demandamount
 FROM flattened
